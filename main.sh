dcf7ada9bfbff4f6ebccd9c230377adcdbecf1cf
vincyxiroff <88449249+vincyxiroff@users.noreply.github.com>
2023-02-09 21:29:31 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -16,7 +16,7 @@ echo "--------------------------------------------------------------------------
 echo ""
 echo "* [0] InstalL warp"
 echo "* [1] install playit"
-echo "* [2] Exit
+echo "* [2] Exit"
 
 read -p "Enter Input (0-2): " input
 
