a0047a5005d8e4f85ece89fc5e294b71425dcbd2
vincyxiroff <88449249+vincyxiroff@users.noreply.github.com>
2023-02-09 21:15:52 +0100

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,27 @@
+if [ "$(id -u)" != "0" ]; then
+   echo "Please Run This Script As A root User" 1>&2
+   exit 1
+fi
+
+apt install sudo -y
+
+echo "-----------------------------------------------------------------------------------"
+echo "#                                                                                  #"
+echo "#                                  VPS SCRIPT                                      #"
+echo "#                                                                                  #"
+echo "#                           Copyright (C) 2023, VPS                                #"
+echo "#                                                                                  #"
+echo "#                                                                                  #"
+echo "-----------------------------------------------------------------------------------#"
+echo ""
+echo "* [0] InstalL warp"
+echo "* [2] Exit
+
+read -p "Enter Input (0- 1): " input
+
+case $input in
+
+    0)
+    apt update
+    apt install curl -y
+    2) echo "Exit" && exit;;
